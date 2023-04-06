af6677722291c3b638eca72840844bdd8a08b495
andyah <ahaguilar134@gmail.com>
2023-04-06 15:39:22 -0600

    changes in components and apis

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,14 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import styles from './pokemons.module.css';
+import { fetchPokemons } from '../api/fetchPokemons';
+import { Pokemon } from '../types/types';
+import LoadingScreen from '../components/LoadingScreen';
+import { waitFor } from '../utils/utils';
 
 const Pokemons = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const [query, setQuery] = useState("");
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+      const fecthAllPokemons = async () => {
+        setIsLoading(true);
+        await waitFor(1000);
+        const allPokemos = await fetchPokemons();
+        setPokemons(allPokemos);
+        setIsLoading(false);
+      }
+      fecthAllPokemons();
+    }, []);
+
+    if( isLoading || !pokemons ){
+        return <LoadingScreen />
+    }
+
+    const filteredPokemons = pokemons?.slice(0, 151).filter( (pokemon) => {
+        return pokemon.name.toLowerCase().match(query.toLowerCase())
+    });
+    
     return (
         <>
         <Header query= { query } setQuery= { setQuery }/>
         <main>
-            <h1>POKEMONS</h1>
+            <nav className={ styles.nav }>
+                { filteredPokemons?.slice(0, 151).map( (pokemon) => (
+                    <Link key={ pokemon.id } className={ styles.listItem }  to={`/pokemons/${pokemon.name.toLowerCase()}`} >
+                    <img className={ styles.listItemIcon } src={ pokemon.imgSrc } alt="bulbasaur" />
+                    <div className={ styles.listItemText } >
+                        <span>{ pokemon.name }</span>
+                        <span>{ pokemon.id }</span>
+                    </div>
+                </Link>
+                ))}
+            </nav>
         </main>
         <Footer />
         </>
